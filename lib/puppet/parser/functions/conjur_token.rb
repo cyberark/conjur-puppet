7571c35a8c8125ca3fdb21f142467cc7c5a8dc3d
Rafał Rzepecki <divided.mind@gmail.com>
2017-01-30 15:59:12 +0100

    Spec conjur_token function

--- lib/puppet/parser/functions/conjur_token.rb
+++ lib/puppet/parser/functions/conjur_token.rb
@@ -8,6 +8,7 @@ module Puppet::Parser::Functions
     uri += "authn/users/" + URI.encode_www_form_component(login) + "/authenticate"
     http = Puppet::Network::HttpPool.http_ssl_instance uri.host, uri.port
     response = http.post uri.request_uri, key
+    raise Net::HTTPError.new response.message, response unless response.code =~ /^2/
     response.body
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,30 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'puppet/network/http/connection'
+require 'puppet/network/http_pool'
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+  end
+end
+
+shared_context "mock conjur connection", conjur: :mock do
+  let(:conjur_connection) do
+    instance_double 'Puppet::Network::HTTP::Connection', 'connection to Conjur'
+  end
+
+  before do
+    allow(Puppet::Network::HttpPool).to receive(:http_ssl_instance) \
+        .with('conjur.test', 443).and_return(conjur_connection)
+  end
+
+  def http_ok body
+    Net::HTTPOK.new('1.1', '200', 'ok').tap do |resp|
+      allow(resp).to receive(:body) { body }
+    end
+  end
+
+  def http_unauthorized
+    Net::HTTPUnauthorized.new '1.1', '403', 'unauthorized'
+  end
+end
