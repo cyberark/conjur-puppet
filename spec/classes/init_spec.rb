ecc4ac6821db5aa45a1b48c64c04edd02712ca2f
Rafał Rzepecki <divided.mind@gmail.com>
2017-01-30 23:10:47 +0100

    Specs for conjur class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,4 +3,29 @@ describe 'conjur' do
   context 'with default values for all parameters' do
     it { should contain_class('conjur') }
   end
+
+  context 'with api key' do
+    let(:params) do {
+      appliance_url: 'https://conjur.test/api',
+      authn_login: 'host/test',
+      authn_api_key: 'the api key',
+    } end
+
+    it "obtains token from the server" do
+      allow_calling_puppet_function(:conjur_token) \
+          .with(['https://conjur.test/api', 'host/test', 'the api key'])\
+          .and_return 'the token'
+      expect(lookupvar('$conjur::token')).to eq 'the token'
+    end
+  end
+
+  context 'with provided token' do
+    let(:params) do {
+      authn_token: 'the provided token'
+    } end
+
+    it "uses the provided token" do
+      expect(lookupvar('$conjur::token')).to eq 'the provided token'
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,3 +33,31 @@ shared_context "mock conjur connection", conjur: :mock do
       'Authorization' => 'Token token="dGhlIHRva2Vu"' # "the token" b64d
   end
 end
+
+module RSpec::Puppet
+  module ClassExampleGroup
+    def environment_module
+      @envmod ||= Puppet::Parser::Functions.environment_module Puppet.lookup(:current_environment)
+    end
+
+    def allow_calling_puppet_function name
+      Puppet::Parser::Functions.function name # to autoload
+      allow_any_instance_of(environment_module).to receive(:"real_function_#{name}")
+    end
+
+    def lookupvar name
+      # HACK There is no way to get to a local variable from the catalog:
+      # by design, catalog contains only the external observable effects.
+      # So instead synthesize a catalog by adding a dummy file
+      # with the contents equal to variable to look up, then get lookup
+      # its content in the catalog. Slow and convoluted, but seems to work.
+      catalog = build_catalog 'test', {}, nil, """
+        #{test_manifest(:class)}
+        file { var:
+          content => #{name}
+        }
+      """, nil
+      catalog.resource('File[var]')[:content]
+    end
+  end
+end
