33ba4459f5920c0203a0d70129140cd9be01b7cd
Rafał Rzepecki <divided.mind@gmail.com>
2017-03-02 04:26:29 +0100

    Make linter shut up
    We should turn off => alignment linting. It's marked as optional
    in the style guide and it's a PITA for me because I'm not using
    monospaced font.

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -11,8 +11,8 @@ node default {
     $secret = conjur::secret('inventory/db-password')
     notify {"Writing this secret to file: ${secret.unwrap}":}
     file { '/tmp/test.pem':
+      ensure  => file,
       content => conjur::secret('inventory/db-password'),
-      ensure => file,
     }
   }
 }
