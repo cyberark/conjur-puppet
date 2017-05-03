cd14a5333688fac4ff014b532137bb0638e5e482
Dustin Collins <dustinrcollins@gmail.com>
2017-05-03 10:17:55 -0500

    Align hashrockets, simplify build/test

--- jenkins.sh
+++ jenkins.sh
@@ -1,56 +1,16 @@
 #!/bin/bash -e
 
-IMAGE_NAME='puppet-test'
+rm -f Gemfile.lock
 
-runInDocker() {
-  cleanUp
-  LAST_CONTAINER=`docker run -d $IMAGE_NAME "$@"`
-  docker logs -f "$LAST_CONTAINER"
-  waitForContainer
-}
+docker build --build-arg PUPPET_VERSION="${PUPPET_VERSION}" -t puppet-test .
 
-waitForContainer() {
-  return `docker wait $LAST_CONTAINER`
-}
+docker run --rm \
+  -v $PWD:/conjur \
+  puppet-test \
+  bash -c 'umask 0000; bundle --quiet && bundle exec rake test'
 
-cleanUp() {
-  if [ -n "$LAST_CONTAINER" ]; then
-    docker rm $LAST_CONTAINER > /dev/null
-    unset LAST_CONTAINER
-  fi
-}
-
-copyOut() {
-  for f in "$@"; do
-    docker cp $LAST_CONTAINER:"/src/$f" "$f"
-  done
-}
-
-rm -f Gemfile.lock  # can screw up ruby env in container
-
-echo "Building Docker image for testing"
-echo "-----"
-docker build -t $IMAGE_NAME .
-
-echo "-----"
-echo "Validating syntax"
-echo "-----"
-runInDocker bundle exec rake validate
-
-echo "-----"
-echo "Linting module"
-echo "-----"
-runInDocker bundle exec rake lint
-
-echo "-----"
-echo "Testing module"
-echo "-----"
-runInDocker bundle exec rake spec
-copyOut rspec.xml
-
-cleanUp
-
-echo "-----"
-echo "Running smoke tests"
-echo "-----"
-./smoketest.sh
+#
+# echo "-----"
+# echo "Running smoke tests"
+# echo "-----"
+# ./smoketest.sh
