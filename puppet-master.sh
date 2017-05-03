d002be73c1f0caceb825410e839c93170df96ae5
Dustin Collins <dustinrcollins@gmail.com>
2017-05-03 12:16:38 -0500

    Move puppet master example files to examples/ dir

--- puppet-master.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-# Launches a Puppet master stack in Docker Compose
-
-docker-compose -f docker-compose.puppet.yml down
-rm -rf puppet puppetdb puppetdb-postgres
-docker-compose -f docker-compose.puppet.yml up -d
