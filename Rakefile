e347bf3ab1eebdc3102cc52e26e4bad49f6e8163
Dustin Collins <dustinrcollins@gmail.com>
2017-05-24 16:57:06 -0500

    Restore release process

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 
 require 'puppetlabs_spec_helper/rake_tasks'
 require 'puppet-lint/tasks/puppet-lint'
+require 'puppet_blacksmith/rake_tasks'
 
 PuppetLint.configuration.relative = true
 
--- release.sh
+++ release.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 # Releases this Puppet module to the Puppet Forge
 
-IMAGE_NAME='puppet-test'
+docker build -t puppet-test .
 
-docker build -t $IMAGE_NAME .
-
-summon docker run --env-file @SUMMONENVFILE --rm -i $IMAGE_NAME bundle exec rake release
+summon docker run --rm -it \
+  -v $PWD:/conjur -w /conjur \
+  --env-file @SUMMONENVFILE \
+  puppet-test \
+  bash -c 'bundle --quiet && bundle exec rake release'
