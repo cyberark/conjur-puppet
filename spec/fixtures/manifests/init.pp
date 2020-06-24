ba1f115071822fea802edc06a7316897733adaf7
Srdjan Grubor <sgnn7@sgnn7.org>
2020-06-24 08:50:07 -0500

    Converted module to a PDK-compatible module
    `pdk convert` was used and all relevant files and fixes were applied to
    make tests pass locally. this does not yet make things work on Jenkins

--- spec/fixtures/manifests/init.pp
+++ spec/fixtures/manifests/init.pp
@@ -2,5 +2,5 @@ class { 'conjur':
   appliance_url => 'https://conjur.test/api',
   authn_token   => Sensitive('the token'),
   account       => 'testacct',
-  version       => $conjur_version
+  version       => $conjur_version,
 }
