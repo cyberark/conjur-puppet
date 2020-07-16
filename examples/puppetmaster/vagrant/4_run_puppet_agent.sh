c61286618aa1e5ce5bb49d12dea041666ad3e06c
Srdjan Grubor <sgnn7@sgnn7.org>
2020-07-16 10:06:06 -0500

    Add ability for Vagrant E2E tests to clear old certs from Puppet 6 server
    Old code used Puppet v5 CLI commands so v6 commands were added to ensure
    that we can clean node certificate from both server versions.

--- examples/puppetmaster/vagrant/4_run_puppet_agent.sh
+++ examples/puppetmaster/vagrant/4_run_puppet_agent.sh
@@ -19,22 +19,33 @@ node_api_key="$(docker exec $conjur_ctl_container conjur host rotate_api_key -h 
 echo "Adding rotated Conjur API key to Windows Credentials Manager"
 vagrant powershell -e -c "/vagrant/add_conjur_creds.ps1 $node_api_key $(conjur_host_port)"
 
-echo "Clearing any previous certs generated for \"$hostname\" from Puppet server"
-master_container=$(docker ps | awk '/puppet\/puppetserver/{print $1}')
+
+echo "Clearing any previous certs generated for \"$hostname\" from Puppet server..."
+puppet_master_container=$(docker ps | awk '/puppet\/puppetserver/{print $1}')
 # Ignore errors since there might not be any certificates to clear
-docker exec "$master_container" puppet cert clean "$hostname.cyber-ark.com" 2>/dev/null || true
+
+# We try first with Puppet v5 CLI (`puppet cert`)
+docker exec "$puppet_master_container" \
+  puppet cert clean "$hostname.cyber-ark.com" &>/dev/null || true
+
+# Then we try the Puppet v6 CLI (`puppetserver ca`). Clean is not very predictable so
+# we also do a revoke before it.
+docker exec "$puppet_master_container" \
+  puppetserver ca revoke --certname "$hostname.cyber-ark.com" 2>/dev/null || true
+docker exec "$puppet_master_container" \
+  puppetserver ca clean --certname "$hostname.cyber-ark.com" 2>/dev/null || true
 
 # When the Puppet server tries to retrieve the API token for host 'node01'
 # from Conjur, it will use the Conjur URL provided by the Puppet agent,
 # which will be of the form 'https://conjur:<random-host-port>'. This random
 # host port is bound to host IPs only, so create an /etc/hosts entry for
 # 'conjur' to point to the Docker Compose gateway on the host.
-gateway_ip="$(docker exec $master_container netstat -rn | grep "0.0.0.0.*UG" | awk '{print $2}')"
+gateway_ip="$(docker exec $puppet_master_container netstat -rn | grep "0.0.0.0.*UG" | awk '{print $2}')"
 
 echo "Adding/modifying /etc/hosts entry in puppet server: \"$gateway_ip conjur\""
 # Make a temporary copy of /etc/hosts to make modifications since `sed` is
 # is not able to directly modify /etc/hosts when it is run via 'docker exec'.
-docker exec $master_container bash -c \
+docker exec $puppet_master_container bash -c \
     "cp /etc/hosts /tmp/hosts; \
      sed -i $'/\tconjur$/d' /tmp/hosts; \
      echo $'$gateway_ip\tconjur' >> /tmp/hosts; \
