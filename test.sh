bcd834bc3e4f298395412d5fd342bf6b887ba03c
Srdjan Grubor <sgnn7@sgnn7.org>
2020-08-13 15:24:01 -0500

    Converted module to a PDK-compatible module
    `pdk convert` was used and all relevant files and fixes were applied to
    make tests pass locally. this does not yet make things work on Jenkins

--- test.sh
+++ test.sh
@@ -3,6 +3,29 @@
 readonly compose_file='docker-compose.test.yml'
 export COMPOSE_PROJECT_NAME="conjur-puppet_$(openssl rand -hex 3)"
 
+checks=( syntax
+         lint
+         metadata_lint
+         check:symlinks
+         check:git_ignore
+         check:dot_underscore
+         check:test_file )
+         # rubocop )
+
+echo "Building the test image..."
 docker-compose -f "$compose_file" build --pull
+
+echo "Sanity checking the plugin..."
+
+for check_type in ${checks[@]}; do
+  echo "- Checking [$check_type]"
+  docker-compose -f "$compose_file" run --rm test-runner \
+    bundle exec rake "$check_type"
+
+  echo "- Checking [$check_type]: OK"
+done
+
+echo "Running specs..."
 docker-compose -f "$compose_file" run --rm test-runner \
-  bundle exec rake test
+  bundle exec rake parallel_spec
+echo "Tests complete!"
