ce42200e7c881054e79ca4d068e91fa7c89cff4b
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2020-08-26 14:27:40 +0100

    E2E tests install the packaged module by default
    The envvar INSTALL_PACKAGED_MODULE has been added to the E2E tests and allows developers to toggle between installing a packaged module or using the module source. The default (which is used by CI) is to install the packaged module.

--- build.sh
+++ build.sh
@@ -6,4 +6,11 @@ docker build -t puppet-pdk -f Dockerfile.pdk .
 docker run --rm \
   -v $PWD:/conjur -w /conjur \
   puppet-pdk \
-  bash -ec 'pdk build --force'
+  bash -ec "
+    pdk build --force
+
+    # Ensure there's a generically named copy of the packaged module. This will allow the
+    # packaged module to be more easily referenced in any automation scripts e.g. integration
+    # tests.
+    cp ./pkg/cyberark-conjur-*.tar.gz ./pkg/cyberark-conjur.tar.gz;
+  "
