144e1a14db6bd415e4d11ce2f78089babbf44d37
Srdjan Grubor <sgnn7@sgnn7.org>
2020-10-05 11:50:53 -0500

    Add ability to pass in authentication token in POST requests
    This will enable us to make post requests that require authorization.
    Even though it's not currently used, it will enable us to invoke API
    endpoints that we were not able to use before.

--- lib/conjur/puppet_module/http.rb
+++ lib/conjur/puppet_module/http.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'conjur/puppet_module/ssl'
+require_relative 'ssl'
 
 module Conjur
   module PuppetModule
@@ -8,42 +8,42 @@ module Conjur
     module HTTP
       class << self
         def get(host_url, path, ssl_certificate, token)
-          uri, use_ssl = parse_url(host_url, path)
-          certs = Conjur::PuppetModule::SSL.load(ssl_certificate)
-
           headers = {}
           if token
             encoded_token = Base64.urlsafe_encode64(token)
             headers['Authorization'] = "Token token=\"#{encoded_token}\""
           end
 
-          Net::HTTP.start uri.host, uri.port, use_ssl: use_ssl, cert_store: certs do |http|
-            response = http.get(uri.request_uri, headers)
-
-            raise Net::HTTPError.new response.message, response unless response.code.match?(%r{^2})
-
-            response.body
+          start_http(host_url, path, ssl_certificate) do |http, uri|
+            http.get(uri.request_uri, headers)
           end
         end
 
-        def post(host_url, path, ssl_certificate, data)
-          uri, use_ssl = parse_url(host_url, path)
+        def post(host_url, path, ssl_certificate, data, token = nil)
+          headers = {}
+          if token
+            headers['Authorization'] = "Token token=\"#{token}\""
+          end
 
-          raise(ArgumentError, "POST data to #{uri} must not be empty!") \
-            if data.nil? || data.empty?
-
-          certs = Conjur::PuppetModule::SSL.load(ssl_certificate)
-
-          Net::HTTP.start uri.host, uri.port, use_ssl: use_ssl, cert_store: certs do |http|
-            response = http.post(uri.request_uri, data)
-
-            raise Net::HTTPError.new response.message, response unless response.code.match?(%r{^2})
-
-            response.body
+          start_http(host_url, path, ssl_certificate) do |http, uri|
+            http.post(uri.request_uri, data, headers)
           end
         end
 
         private
+
+        def start_http(host_url, path, ssl_certificate)
+          uri, use_ssl = parse_url(host_url, path)
+          certs = Conjur::PuppetModule::SSL.load(ssl_certificate)
+
+          Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl, cert_store: certs) do |http|
+            response = yield http, uri
+
+            raise Net::HTTPError.new response.message, response unless response.code.match?(%r{^2})
+
+            response.body
+          end
+        end
 
         def parse_url(url, path)
           url += '/' unless url.end_with? '/'
