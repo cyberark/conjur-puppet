7afd6d1fe971f8ea4e189acdeaaeab70027a3eb1
Srdjan Grubor <sgnn7@sgnn7.org>
2020-11-23 10:27:09 -0600

    Add workaround for broken upstream PDK image
    This workaround for cyberark/conjur-puppet/issues/246 ensures
    e our own PDK image since PDK image breaks on the first run
    while not affecting the subsequent runs.

--- ci/test.sh
+++ ci/test.sh
@@ -3,12 +3,17 @@
 # To ensure this script always executes relative to the repo root
 cd "$(dirname "$0")/.."
 
+docker build -t puppet-pdk -f ./ci/Dockerfile.pdk .
+
 if [ ! "${SKIP_VALIDATION}" == "true" ]; then
   echo "Running validations..."
   docker run --rm \
     -v $PWD:/root \
     -w /root \
-    puppet/pdk validate control-repo,metadata,puppet,ruby,yaml
+    puppet-pdk \
+    bash -ec "
+      pdk validate control-repo,metadata,puppet,ruby,yaml
+    "
 fi
 
 echo "Running specs..."
@@ -16,5 +21,9 @@ mkdir -p ./spec/output
 docker run --rm \
   -v $PWD:/root \
   -w /root \
-  puppet/pdk test unit --format=junit:./spec/output/rspec.xml --format=text
+  puppet-pdk \
+  bash -ec "
+    pdk test unit --format=junit:./spec/output/rspec.xml --format=text
+  "
+
 echo "Tests complete!"
