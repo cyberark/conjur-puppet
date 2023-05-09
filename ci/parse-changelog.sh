eccdd2b5b7b8d59d3c1fab0c73ee5ed8296f78a9
junior.taeza <junior.taeza@cyberark.com>
2023-05-09 13:31:18 -0400

    Updated Jenkins pipeline to use parseChangelog utility

--- ci/parse-changelog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-# To ensure this script always executes relative to the repo root
-cd "$(dirname "$0")/.."
-
-docker run --rm \
-  --volume "${PWD}/CHANGELOG.md:/CHANGELOG.md"  \
-  cyberark/parse-a-changelog
